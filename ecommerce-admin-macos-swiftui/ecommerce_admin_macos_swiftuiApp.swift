b9d58e3a151f3fea87d6243dccbb69effb3e0504
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-04-13 08:20:16 +0700

    update fixing main file

--- ecommerce-admin-macos-swiftui/ecommerce_admin_macos_swiftuiApp.swift
+++ ecommerce-admin-macos-swiftui/ecommerce_admin_macos_swiftuiApp.swift
@@ -8,49 +8,25 @@ import SwiftUI
 
 @main
 struct ecommerce_admin_macos_swiftuiApp: App {
-    
-    let window = NSWindow(
-        contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-        styleMask: [
-            .titled,
-            .closable,
-            .miniaturizable,
-//            .resizable
-        ],
-        backing: .buffered,
-        defer: false)
-    
-    
+
     let settings = PublishedConstants()
 
-   
      var body: some Scene {
         WindowGroup{
-        
             VStack {
                 ContentView().environmentObject(settings)
                     .onAppear {
                         let _ = NSApplication.shared.windows.map { $0.tabbingMode = .disallowed }
-                        
-                        let _ = NSApplication.shared.windows.map { $0.styleMask = [ .titled,
-                                                                                    .closable,
-                                                                                    .miniaturizable]}
-                    }
+                        let _ = NSApplication.shared.windows.map { $0.styleMask = [ .titled,.closable,.miniaturizable]}
+                        }
                     }
                 }
-        
         .windowStyle(HiddenTitleBarWindowStyle())
-        .commands{
-            SidebarCommands()
-        }
         .commands {
-                CommandGroup(replacing: .newItem) {
-              }
-            }
-        .commands{
+            SidebarCommands()
+            CommandGroup(replacing: .newItem) {}
             ToolbarCommands()
-        }
-       
+            }
     }
 }
 
